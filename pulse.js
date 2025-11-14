7c8a6b00cc61d8249f98fdef9a35a75967bc43ce
Ritu Raj Sharma <riturajsharma@Ritus-MacBook-Air.local>
2025-11-13 19:38:58 -0500

    Added the initial code

---
+++ pulse.js
@@ -0,0 +1,44 @@
+var theme = ""
+const toggle = document.getElementById('toggle');
+
+window.addEventListener('load', ()=>{
+
+});
+  
+  toggle.addEventListener('change', changetheme);
+
+  function changetheme(){
+    if (toggle.checked) {
+      theme = "dark";
+      dark();
+    } else {
+      theme = "light";
+      light();
+    }
+  }
+
+  function bodytoggle(){
+    document.getElementById("darklight").classList.toggle("light");
+    document.getElementById("darklight").classList.toggle("dark");
+  }
+  function dark(){
+    document.getElementById("logoimg").src="img/dark_logo.png";
+    if( document.getElementById("heroimg")!=null)
+    document.getElementById("heroimg").src = document.getElementById("heroimg").src.replace("light",theme);
+    bodytoggle();
+  }
+  function light(){
+    document.getElementById("logoimg").src="img/light_logo.png";
+    if( document.getElementById("heroimg")!=null)
+    document.getElementById("heroimg").src = document.getElementById("heroimg").src.replace("dark",theme);
+    bodytoggle();
+  }
+
+  //overlay
+  function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
